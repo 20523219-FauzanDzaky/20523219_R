0676c3db81991504a3da80dcb56149a50557325c
20523219-FauzanDzaky <80076651+20523219-FauzanDzaky@users.noreply.github.com>
2021-05-29 21:28:16 +0700

    Add files via upload

---
+++ 20523219_R.R
@@ -0,0 +1,10 @@
+yac_expr("D(x) 2*x^5")
+yac_expr("D(x) x^2+4")
+yac_expr("D(x) x^5-6*x^7")
+
+yac_expr("D(x) (x^6-2)*(Sin(x)-4)")
+yac_expr("D(x) (sin(x)-4)*(x^6-2)")
+yac_expr("D(x) (x^6-2)/(Sin(x)-4)")
+yac_expr("D(x) (sin(x)-4)/(x^6-2)")
+yac_expr("D(x) (sin(x)-4)^6-2")
+yac_expr("D(x) sin(x^6-2)-4")
